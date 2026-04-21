5fa3196fee2d6f7acdf556c90d711ff36cd0d262
Nithis kumar K <nithis2000june@gmail.com>
2026-04-21 15:42:03 +0530

    ta change

--- sfx.py
+++ sfx.py
@@ -1,5 +1,6 @@
-import pandas as pd,numpy as np, matplotlib.pyplot as plt, seaborn as sns, mplfinance as mpf, talib as ta, time
+import pandas as pd,numpy as np, matplotlib.pyplot as plt, seaborn as sns, time
 import ccxt
+import pandas_ta as ta
 from xgboost import XGBRegressor
 
 
@@ -23,8 +24,8 @@ def create_feature(df):
     df["volatility_stat"] = df["return"].rolling(20).std()
     df["volatility_range"] = (df["high"] - df["low"]) / df["close"]
     df["volatility_ratio"] =df["volatility_range"] /df["volatility_stat"]
-    df["rsi"] = ta.RSI(df["close"], timeperiod=14)
-    df["atr"] = ta.ATR(df["high"], df["low"], df["close"], timeperiod=14)
+    df["rsi"] = ta.rsi(df["close"], length=14)
+    df["atr"] = ta.atr(df["high"], df["low"], df["close"], length=14)
     df["vol_mean"] = df["volume"].rolling(20).mean()
     df["vol_std"] = df["volume"].rolling(20).std()
     df["volume_z"] = (df["volume"] - df["vol_mean"]) / df["vol_std"]
