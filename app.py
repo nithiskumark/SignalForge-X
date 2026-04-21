215cd23ec77126b301c372128e66345a990f64d3
Nithis kumar K <nithis2000june@gmail.com>
2026-04-21 15:31:12 +0530

    ml app

---
+++ app.py
@@ -0,0 +1,69 @@
+import streamlit as st
+from sfx import get_data, create_feature, predict_future, train_model
+import matplotlib.pyplot as plt, pandas as pd
+
+st.set_page_config(
+    page_title="SignalForge-X",
+    layout="wide"
+)
+
+st.title("SIGNALFORGE-X")
+exchange = "binance"
+coin = st.text_input("Write the Coin you want to predict eg.BTC/USDT")
+tf = "5m"
+
+@st.cache_data(ttl=120)  
+def load_data(exchange, coin, tf):
+    return get_data(exchange, coin, tf)
+
+@st.cache_data
+def feature(df):
+    return create_feature(df)
+
+@st.cache_resource
+def ml(df, steps):
+    return predict_future(df, steps)
+@st.cache_resource
+def ml2(df, steps):
+    return train_model(df, steps)
+
+if exchange and coin and tf:
+    df = load_data(exchange,coin,tf)
+    st.subheader("Sample Data")
+    st.dataframe(df.tail())
+    df = feature(df)
+    steps = st.number_input("Enter the no of preds", min_value=1, step=1)
+    if st.button("ML01"):
+        if steps:
+            y_f = ml(df, steps)
+            st.write(y_f)
+            actual = df["close"].tail(25)
+            last_time = df.index[-1]
+            future_index = pd.date_range(start=last_time,periods=len(y_f) + 1,freq="5min")[1:]
+            pred_series = pd.Series(y_f, index=future_index)
+            fig, ax = plt.subplots(figsize=(12,6))
+            ax.plot(actual.index, actual.values, label="Actual")
+            ax.plot(pred_series.index, pred_series.values, '--', label="Predicted")
+            ax.legend()
+            ax.set_title("Actual vs Predicted")
+            st.pyplot(fig)
+    if st.button("ML02"):
+        if steps:
+            y_f = ml2(df, steps)
+            st.write(y_f)
+            actual = df["close"].tail(25)
+            last_time = df.index[-1]
+            future_index = pd.date_range(start=last_time,periods=len(y_f) + 1,freq="5min")[1:]
+            pred_series = pd.Series(y_f, index=future_index)
+            fig, ax = plt.subplots(figsize=(12,6))
+            ax.plot(actual.index, actual.values, label="Actual")
+            ax.plot(pred_series.index, pred_series.values, '--', label="Predicted")
+            ax.legend()
+            ax.set_title("Actual vs Predicted")
+            st.pyplot(fig)
+
+
+
+
+
+
---
+++ sfx.py
@@ -0,0 +1,92 @@
+import pandas as pd,numpy as np, matplotlib.pyplot as plt, seaborn as sns, mplfinance as mpf, talib as ta, time
+import ccxt
+from xgboost import XGBRegressor
+
+
+def get_data(exchange, coin, tf):
+    exc = getattr(ccxt, exchange)()
+    data = exc.fetch_ohlcv(coin, tf)
+    df = pd.DataFrame(data, columns=[
+        'timestamp','open','high','low','close','volume'])
+    df["datetime"] = (pd.to_datetime(df["timestamp"], unit='ms', utc=True)
+    .dt.tz_convert('Asia/Kolkata'))
+    df = df.drop("timestamp",axis=1)
+    df.set_index("datetime",inplace=True)
+
+    return df
+
+def create_feature(df):
+    df["ema_9"] = df["close"].ewm(span=9).mean()
+    df["ema_21"] = df["close"].ewm(span=21).mean()
+    df["ema_50"] = df["close"].ewm(span=50).mean()
+    df["return"] = df["close"].pct_change()
+    df["volatility_stat"] = df["return"].rolling(20).std()
+    df["volatility_range"] = (df["high"] - df["low"]) / df["close"]
+    df["volatility_ratio"] =df["volatility_range"] /df["volatility_stat"]
+    df["rsi"] = ta.RSI(df["close"], timeperiod=14)
+    df["atr"] = ta.ATR(df["high"], df["low"], df["close"], timeperiod=14)
+    df["vol_mean"] = df["volume"].rolling(20).mean()
+    df["vol_std"] = df["volume"].rolling(20).std()
+    df["volume_z"] = (df["volume"] - df["vol_mean"]) / df["vol_std"]
+    df["volume_spike"] = (df["volume"] > df["vol_mean"] * 1.5).astype(int)
+    df["resistance"] = df["high"].rolling(20).max()
+    df["support"] = df["low"].rolling(20).min()
+    df["dist_to_resistance"] = df["resistance"] - df["close"]
+    df["dist_to_support"] = df["close"] - df["support"]
+    df["dist_res_norm"] = df["dist_to_resistance"] / df["atr"]
+    df["dist_sup_norm"] = df["dist_to_support"] / df["atr"]
+
+    return df
+
+def train_model(df, steps):
+    df["future_close"] = df["close"].shift(-steps)
+    features = ["ema_9", "ema_21", "ema_50", "return", "volatility_range", "volatility_ratio", "rsi", 
+            "atr", "volume_z", "volume_spike", "dist_res_norm", "dist_sup_norm"]
+    preds = []
+    X = df[features]
+    y = df["future_close"]
+    X = X[:-steps]
+    y = y[:-steps]
+    X_f = X[-steps:]
+    model = XGBRegressor(
+    n_estimators=300,
+    max_depth=6,
+    learning_rate=0.05)
+    model.fit(X, y)
+    y_f = model.predict(X_f)
+
+    return y_f
+
+def predict_future(df,steps):
+    features = ["ema_9", "ema_21", "ema_50", "return", "volatility_range",
+                "volatility_ratio", "rsi", "atr", "volume_z",
+                "volume_spike", "dist_res_norm", "dist_sup_norm"]
+
+    df_temp = df.copy()
+    preds = []
+    X = df[features]
+    y = df["close"]
+    model = XGBRegressor(n_estimators=300, max_depth=6, learning_rate=0.05)
+    model.fit(X,y)
+
+    for _ in range(steps):
+        X_f = df_temp[features].iloc[-1:]
+        pred = model.predict(X_f)[0]
+        preds.append(pred)
+
+        new_row = df_temp.iloc[-1:].copy()
+        new_row["close"] = pred
+
+        df_temp = pd.concat([df_temp, new_row])
+        df_temp = create_feature(df_temp)
+
+    return np.array(preds)
+
+
+
+
+
+
+    
+
+
